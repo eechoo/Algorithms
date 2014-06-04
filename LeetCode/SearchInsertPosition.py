9bd1e2f54908f9b529b574c31157c9efef3adaba
yecui <yecui@baidu.com>
2014-06-04 11:05:33 +0800

    Add SearchIsertion

--- LeetCode/SearchInsertPosition.py
+++ LeetCode/SearchInsertPosition.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python
 class Search(object): 
+    # @param A, a list of integers
+    # @param target, an integer to be inserted
+    # @return integer
+    def searchInsert(self, A, target):
+        if len(A)==0:
+            return 0
+        for i in range(0,len(A)):
+            if(target == A[i] or target < A[i]):
+                return i
+        return len(A)
 
 
 def test(got, expected):
@@ -11,11 +21,10 @@ def test(got, expected):
 
 def main():
     ins=Search()
-    test(ins.numTrees(3),5)
-    test(ins.numTrees(4),14)
-    test(ins.numTrees(5),42)
-    test(ins.numTrees(1),1)
-    test(ins.numTrees(2),2)
+    test(ins.searchInsert([1,3,5,6], 5),2)
+    test(ins.searchInsert([1,3,5,6], 2),1)
+    test(ins.searchInsert([1,3,5,6], 7),4)
+    test(ins.searchInsert([1,3,5,6], 0),0)
 
 if __name__ == '__main__':
     main()
